baa23339a72bb72f9babc92220e181d0743b6151
JDR204 <jreynolds204@yahoo.com>
2023-09-11 05:53:34 -0500

    Add function to randomly generate choice
    A function has been added to randomly assign the computer
    a choice for the rock, paper, scissors game.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,12 @@
-console.log("Hello!");
+function getComputerChoice() {
+    num = Math.floor(Math.random() * 3);
+    if (num == 0) {
+        return "Paper";
+    }   else if (num == 1) {
+        return "Rock";
+    }   else {
+        return "Scissors";
+    }
+}
+let choice = getComputerChoice();
+console.log(choice);
